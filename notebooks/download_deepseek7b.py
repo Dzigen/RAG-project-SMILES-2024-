9883eff6507cd7be0ef90d3f543fce002a74e8fd
dizgen <dzigen@gmail.com>
2025-07-13 18:48:53 +0300

    add independent scripts for model loading

---
+++ notebooks/download_deepseek7b.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import  pipeline
+import torch
+import gc
+
+gc.collect()
+torch.cuda.empty_cache()
+
+SAVE_MODEL_PATH = "../models/deepseek-ai/deepseek-llm-7b-chat" 
+LOAD_MODEL = "deepseek-ai/deepseek-llm-7b-chat"  
+
+access_token = "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"
+
+tokenizer = AutoTokenizer.from_pretrained(LOAD_MODEL, token=access_token)
+model = AutoModelForCausalLM.from_pretrained(LOAD_MODEL, torch_dtype=torch.bfloat16, token=access_token).to('cpu')
+
+model.save_pretrained(SAVE_MODEL_PATH)
+tokenizer.save_pretrained(SAVE_MODEL_PATH)
---
+++ notebooks/download_gemma7b.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import  pipeline
+import torch
+import gc
+
+gc.collect()
+torch.cuda.empty_cache()
+
+SAVE_MODEL_PATH = "../models/google/gemma-7b-it" 
+LOAD_MODEL = "google/gemma-7b-it"  
+
+access_token = "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"
+
+tokenizer = AutoTokenizer.from_pretrained(LOAD_MODEL, token=access_token)
+model = AutoModelForCausalLM.from_pretrained(LOAD_MODEL, torch_dtype=torch.bfloat16, token=access_token).to('cpu')
+
+model.save_pretrained(SAVE_MODEL_PATH)
+tokenizer.save_pretrained(SAVE_MODEL_PATH)
