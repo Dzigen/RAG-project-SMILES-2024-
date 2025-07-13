5c938277640b87dde858061aedf0a55bef25a500
dizgen <dzigen@gmail.com>
2025-07-13 21:23:18 +0300

    quick fix

--- experiments/reader_context_noising_with_scores (exp #5)/with_relevant_contexts_without_score (exp #5.4)/run_all_configs.sh
+++ experiments/reader_context_noising_with_scores (exp #5)/with_relevant_contexts_without_score (exp #5.4)/run_all_configs.sh
@@ -6,8 +6,8 @@ echo $available_configs
 
 for current_config in $available_configs
 do
-    config_path="$base_dir/configs/$current_config"
+    config_path="./configs/$current_config"
     echo "Current config: $config_path"
-    python3 "$base_dir/run_config.py" "$config_path"
+    python3 run_config.py $config_path
 
 done
